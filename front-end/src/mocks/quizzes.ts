35bca043a423a9217cf807192c0a757d507765d9
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2024-04-06 18:16:11 +0200

    feat: including minigames in the quiz and simon enhancement
    - Adding the Simon and Memory games to the quiz logic #48
    - Adding the Simon tip (repeat the sequence after a certain amount of time) #50
    - Adding colors to the Simon (can be disabled) #49
    - Adding customizations for the Simon in the quiz config (Nb of rounds / Boxes / errors before skipping) #49
    - Replacing the sound of the Simon button #49
    - Reshaping the Simon game interface #49
    - Handling the end of the Simon and Memory to the end of the quiz #48
    
    Closes: #48, #49, #50

--- front-end/src/mocks/quizzes.ts
+++ front-end/src/mocks/quizzes.ts
@@ -4,6 +4,7 @@ export const quizzes: IQuiz[]= [{
   "title": "Capitales et géographie",
   "quizDescription": "Testez vos connaissances sur les capitales et la géographie",
   "questions": [1,2,3,4,5,6],
+  "specials": [{name: "Simon", rules: {numberOfRound: 5, numberOfBoxes: 4, numberOfRetriesAllowed: 2}}, {name: "Memory", rules: {}}],
   "imageUrl": "https://previews.123rf.com/images/yupiramos/yupiramos2203/yupiramos220302216/183817525-mati%C3%A8re-scolaire-de-g%C3%A9ographie.jpg",
   "quizId": 1
 },
@@ -11,6 +12,7 @@ export const quizzes: IQuiz[]= [{
     "title": "Acteurs et actrices",
     "quizDescription": "Testez vos connaissances sur les acteurs et actrices",
     "questions": [7,8,9,10,11],
+    "specials": [],
     "imageUrl": "https://img.freepik.com/photos-gratuite/outils-sport_53876-138077.jpg?size=626&ext=jpg&ga=GA1.1.1457017613.1710413038&semt=sph",
     "quizId": 2
   },
@@ -18,6 +20,7 @@ export const quizzes: IQuiz[]= [{
     "title": "Musique",
     "quizDescription": "Testez vos connaissances sur la musique",
     "questions": [12,13,14,15,16],
+    "specials": [{name: "Simon", rules: {numberOfRound: 5}}],
     "imageUrl" : "https://images.rtl.fr/~c/770v513/rtl/www/1076683-leonardo-dicaprio-est-l-un-des-acteurs-les-plus-payes-avec-45-millions-de-dollars-entre-2013-et-2014.jpg",
     "quizId": 3
   },
@@ -25,6 +28,7 @@ export const quizzes: IQuiz[]= [{
     "title": "Sport",
     "quizDescription": "Testez vos connaissances sur le sport",
     "questions": [17,18,19,20,21],
+    "specials": [{name: "Memory", rules: {}}],
     "imageUrl" : "https://www.pourquoidocteur.fr/media/article/COPY_istock-1213864843-1625666281.jpg",
     "quizId": 4
   }
