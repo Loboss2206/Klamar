51869597f07753d0cda34fa0b93fc9c2fb770ca9
Momen Takroun <momen.takroun@etu.univ-cotedazur.fr>
2024-04-06 22:42:02 +0200

    feat: add user creator Page with FormGroup
    This add the user creator Page with the FormGroup of Angular + this
    retrieve the data from the input to be ready to be send to the database
    
    Closes #47

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -18,26 +18,26 @@ import { MemoryItemComponent } from "../components/memory-item/memory-item.compo
 import { UserCreatorComponent } from 'src/components/userManager/userCreator.component';
 import { HowToPlayQuestionComponent } from 'src/components/howToPlayQuestion/howToPlayQuestion.component';
 import { ZoomSliderComponent } from 'src/components/zoomSlider/zoomSlider.component';
-import {NavbarComponent} from "../components/navbar/navbar.component";
-import {RouterModule, Routes} from "@angular/router";
-import {FelicidadComponent} from "../components/felicidad/felicidad.component";
-import {GraphicPageComponent} from "../components/graphic-page/graphic-page.component"
-import {StatMemoyPageComponent} from "../components/stat-memoy-page/stat-memoy-page.component";
-import {StatSimonPageComponent} from "../components/stat-simon-page/stat-simon-page.component";
-import {StatQuestionPageComponent} from "../components/stat-question-page/stat-question-page.component";
-import {GraphicComponent} from "../components/graphic/graphic.component";
-import {SimonGameComponent} from "../components/simon-game/simon-game.component";
+import { NavbarComponent } from "../components/navbar/navbar.component";
+import { RouterModule, Routes } from "@angular/router";
+import { FelicidadComponent } from "../components/felicidad/felicidad.component";
+import { GraphicPageComponent } from "../components/graphic-page/graphic-page.component"
+import { StatMemoyPageComponent } from "../components/stat-memoy-page/stat-memoy-page.component";
+import { StatSimonPageComponent } from "../components/stat-simon-page/stat-simon-page.component";
+import { StatQuestionPageComponent } from "../components/stat-question-page/stat-question-page.component";
+import { GraphicComponent } from "../components/graphic/graphic.component";
+import { SimonGameComponent } from "../components/simon-game/simon-game.component";
 
 
 const routes: Routes = [
-  { path: '', component: QuizSelectorComponent , data:{title: 'Sélection du quiz'}},
-  { path: 'quizSelector', component: QuizSelectorComponent, data:{title: 'Sélection du quiz'}},
-  { path: 'quiz', component: QuizComponent,data:{ title: 'Quiz'}},
-  { path: 'felicitations', component: FelicidadComponent,data:{ title: 'Félicitations'}},
-  { path: 'quizResultPage/:id', component: quizResultPageComponent,data:{ title: 'Résultat du quiz'}},
-  { path: 'howToPlayQuestion', component: HowToPlayQuestionComponent, data:{ title: 'Comment jouer'}},
-  { path: 'simon/:numberOfRound', component: SimonGameComponent, data: { title: 'Simon' }},
-  { path: 'memory' component: MemoryContainerComponent, data: { title: 'Memory' }}
+  { path: '', component: QuizSelectorComponent, data: { title: 'Sélection du quiz' } },
+  { path: 'quizSelector', component: QuizSelectorComponent, data: { title: 'Sélection du quiz' } },
+  { path: 'quiz', component: QuizComponent, data: { title: 'Quiz' } },
+  { path: 'felicitations', component: FelicidadComponent, data: { title: 'Félicitations' } },
+  { path: 'quizResultPage/:id', component: quizResultPageComponent, data: { title: 'Résultat du quiz' } },
+  { path: 'howToPlayQuestion', component: HowToPlayQuestionComponent, data: { title: 'Comment jouer' } },
+  { path: 'simon/:numberOfRound', component: SimonGameComponent, data: { title: 'Simon' } },
+  { path: 'memory', component: MemoryContainerComponent, data: { title: 'Memory' } }
 ];
 
 @NgModule({
@@ -57,7 +57,7 @@ const routes: Routes = [
     QuizSelectorContainerComponent,
     QuizSelectorComponent,
     MemoryContainerComponent,
-    MemoryItemComponent
+    MemoryItemComponent,
     UserCreatorComponent,
     QuizSelectorComponent,
     NavbarComponent,
--- front-end/src/components/userManager/userCreator.component.ts
+++ front-end/src/components/userManager/userCreator.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { ButtonComponent } from '../quizButton/button.component';
 import { ZoomSliderComponent } from '../zoomSlider/zoomSlider.component';
-import { textInputComponent } from '../textInput/textInput.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { GenericButtonComponent } from '../genericButton/genericButton.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,16 +11,42 @@ import { GenericButtonComponent } from '../genericButton/genericButton.component
   standalone: true,
   imports: [
     ButtonComponent,
-    ZoomSliderComponent, textInputComponent, NavbarComponent,
-    GenericButtonComponent
+    ZoomSliderComponent, NavbarComponent,
+    GenericButtonComponent,
+    ReactiveFormsModule
+
   ],
   templateUrl: './userCreator.component.html',
   styleUrl: './userCreator.component.scss'
 })
 export class UserCreatorComponent {
+  public userCreatorForm: FormGroup;
 
 
-  constructor() {
+  constructor(public formBuilder: FormBuilder) {
+
+    this.userCreatorForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      alzheimerState: [3, Validators.required],
+      userBirth: ['', Validators.required],
+      hobbies: ['', Validators.required],
+      baseZoom: [100, Validators.required],
+      choiceSimon: ['', Validators.required],
+      choiceRead: ['', Validators.required]
+    });
   }
 
+
+  isFormValid(): boolean {
+    return this.userCreatorForm.valid;
+  }
+
+
+  addUser(): void {
+    console.log(this.userCreatorForm.getRawValue());
+  }
+
+
 }
+
