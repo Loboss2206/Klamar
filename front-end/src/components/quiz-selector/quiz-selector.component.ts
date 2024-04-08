00e266ec4691ff66f174a99342d727b1fb13d9ff
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2024-04-08 14:03:56 +0200

    feat: Adding the possibility to search for a quiz on the selector page

--- front-end/src/components/quiz-selector/quiz-selector.component.ts
+++ front-end/src/components/quiz-selector/quiz-selector.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { TagsSelectorComponent } from '../tags-selector/tags-selector.component';
 import { SearchQuizSelectorComponent } from '../search-quiz-selector/search-quiz-selector.component';
 import { QuizSelectorContainerComponent } from '../quiz-selector-container/quiz-selector-container.component';
+import {QuizService} from "../../services/quiz-service.service";
+import IQuiz from "../../interfaces/IQuiz";
 
 @Component({
     selector: 'app-quiz-selector',
@@ -15,4 +17,20 @@ import { QuizSelectorContainerComponent } from '../quiz-selector-container/quiz-
     styleUrl: './quiz-selector.component.scss'
 })
 
-export class QuizSelectorComponent { }
+export class QuizSelectorComponent {
+
+  quizzes: IQuiz[] = [];
+
+
+  constructor(private quizService: QuizService) {
+  }
+
+  ngOnInit(): void {
+    this.quizzes = this.quizService.getQuizzes();
+  }
+
+  searchQuizzes(searchTerm: string) {
+    this.quizzes = this.quizService.searchQuizzes(searchTerm);
+  }
+
+}
