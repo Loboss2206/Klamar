a34d0911d879d9565356b7e6e45f82822fb79e84
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2024-04-12 15:41:12 +0200

    refactor: refactoring some css
    Migrating to css variables
    Making a better zoom slider
    Adding a background image
    
    Closes: #81

--- front-end/src/components/zoomSlider/zoomSlider.component.spec.ts
+++ front-end/src/components/zoomSlider/zoomSlider.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ZoomSliderComponent } from './zoomSlider.component';
+import {ZoomSliderComponent} from './zoomSlider.component';
 
 describe('ZoomSliderComponent', () => {
   let component: ZoomSliderComponent;
