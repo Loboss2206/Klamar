8e90d4cd94af10b4588c7714e1f5ff51c716d3b8
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-05-06 20:34:08 +0200

    feat: add routes to manage the users #127

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,16 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+const UserConfig = require('./user-config.model.js')
+
+module.exports = new BaseModel('User', {
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+    firstname: Joi.string().required(),
+    birthdate: Joi.string().required(),
+    hobbies: Joi.string().required(),
+    config: UserConfig.schema,
+    charts: Joi.array().items(Joi.string()).required(),
+    statsId: Joi.array().items(Joi.number()).required(),
+    avatar: Joi.string().required(),
+    colorBlind: Joi.string().required()
+})
