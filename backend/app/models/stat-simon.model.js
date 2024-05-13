82e1bd0335db535b4140967491d2b49a17bdd98a
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-05-13 13:35:37 +0200

    feat: add requests to the API for the stats #135

---
+++ backend/app/models/stat-simon.model.js
@@ -0,0 +1,10 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('StatSimon', {
+    erreurSimon: Joi.number().required(),
+    indicesSimon: Joi.number().required(),
+    tempsSimon: Joi.number().required(),
+    tailleFinalSimon: Joi.number().required(),
+    nombreDeCouleurs: Joi.number().required(),
+})
---
+++ backend/app/models/stat.model.js
@@ -0,0 +1,18 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+const StatSimon = require('./stat-simon.model.js')
+const StatMemory = require('./stat-memory.model.js')
+const StatQuestion = require('./stat-question.model.js')
+
+module.exports = new BaseModel('Stat', {
+  id: Joi.number().required(),
+
+  simonStats: StatSimon.schema,
+  memoryStats: StatMemory.schema,
+  questions: StatQuestion.schema,
+
+  sucessSimon: Joi.number().required(),
+  sucessMemory: Joi.number().required(),
+  sucessQuiz: Joi.number().required(),
+  date: Joi.string().required(),
+})
