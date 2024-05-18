24b966995575be7e3de5b406ca8b8847080e6f31
NurdinAymeric <147172361+donDflux@users.noreply.github.com>
2024-05-18 19:50:25 +0200

    feat : changing_Structure_For_Stats

--- backend/app/models/stat-question.model.js
+++ backend/app/models/stat-question.model.js
@@ -2,11 +2,11 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('StatQuestion', {
-    idQuestion: Joi.number().required(),
-    pointQuestion: Joi.number().required(),
-    maxPointQuestion: Joi.number().required(),
-    erreurQuiz: Joi.number().required(),
-    indicesQuiz: Joi.number().required(),
-    tempsQuiz: Joi.number().required(),
-    reponseId: Joi.array().items(Joi.number()).required()
+  idQuestion: Joi.number().required(),
+  pointQuestion: Joi.number().required(),
+  maxPointQuestion: Joi.number().required(),
+  erreurQuiz: Joi.number().required(),
+  indicesQuiz: Joi.number().required(),
+  tempsQuiz: Joi.number().required(),
+  reponseId: Joi.array().items(Joi.number()).required(),
 })
--- front-end/src/services/stats.service.ts
+++ front-end/src/services/stats.service.ts
@@ -4,14 +4,19 @@ import IStats from "../interfaces/IStats";
 import {stats} from "../mocks/stats";
 import IUser from "../interfaces/IUser";
 import IAdmin from "../interfaces/IAdmin";
+import {HttpClient} from "@angular/common/http";
+import {serverUrl} from "../configs/server.config";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
+  private apiURL = serverUrl + '/stats'
+  constructor(private http : HttpClient) {
+  }
   private currentStatId: number | undefined;
   getStat(id: number | undefined) : Observable<IStats | undefined>{
-    return of(stats.find(stats => stats.id === id))
+    return this.http.get<IStats>(this.apiURL + `/${id}`)
   }
   getTheStat(): number | undefined {
     return this.currentStatId;
